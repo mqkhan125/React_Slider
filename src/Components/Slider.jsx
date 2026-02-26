46903d8629827d44d7dbb8c038d0130370a4cc43
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-26 22:50:24 +0500

    feat(slider): add autoplay functionality with pause on hover

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import data from "./JsonData.json";
 
 const Slider = () => {
   const [next, setNext] = useState(0);
+  const intervalRef = useRef(null);
+
+  // start autoplay
+  const startSlider = () => {
+    intervalRef.current = setInterval(() => {
+      setNext((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    },2000);
+  };
+
+  // stop autoplay
+  const stopSlider = () => {
+    clearInterval(intervalRef.current)
+  }
 
   const handleNext = () => {
     setNext((prev) => (prev === data.length - 1 ? 0 : prev + 1));
@@ -12,8 +25,19 @@ const Slider = () => {
     setNext((prev) => (prev === 0 ? data.length - 1 : prev - 1));
   };
 
+  useEffect(() => {
+    startSlider();
+    return(() => {
+        clearInterval(intervalRef.current)
+    })
+  }, []);
+
   return (
-    <div className="slider-container">
+    <div
+      className="slider-container"
+      onMouseEnter={stopSlider}
+      onMouseLeave={startSlider}
+    >
       <div className="slider-wrapper">
         <img
           src={data[next]?.download_url}
